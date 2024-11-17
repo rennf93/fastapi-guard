ed2eb37a8983810e2c2c6d0e479b226734c1732b
Renn F <rennf93@gmail.com>
2024-11-17 02:32:28 +0100

    Bump version - prepare for merge into master

--- setup.py
+++ setup.py
@@ -8,25 +8,28 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="fastapi_guard",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     install_requires=[
         "aiohttp",
         "cachetools",
         "fastapi",
         "ipaddress",
-        "IP2Location",
+        "maxminddb",
         "requests",
         "uvicorn",
     ],
     extras_require={
         "dev": [
+            "black",
+            "flake8",
             "httpx",
             "pytest",
             "pytest-asyncio",
             "pytest-mock",
         ],
     },
+    python_requires=">=3.10",
     author="Renzo Franceschini",
     author_email="rennf93@gmail.com",
     description="A security library for FastAPI to control IPs, log requests, and detect penetration attempts.",
@@ -34,10 +37,18 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/rennf93/fastapi-guard",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Framework :: FastAPI",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Internet :: WWW/HTTP :: API :: Middleware",
+        "Topic :: Security",
     ],
-    python_requires=">=3.10",
     include_package_data=True,
+    package_data={
+        "fastapi_guard": ["py.typed"],
+    },
 )
