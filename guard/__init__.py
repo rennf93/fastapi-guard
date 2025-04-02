bf8ac59f75d20e17941922d703b06e1c467ef125
Renn F <rennf93@gmail.com>
2025-04-03 01:17:50 +0200

    Sorted imports (__init__.py)

--- guard/__init__.py
+++ guard/__init__.py
@@ -1,9 +1,9 @@
 # fastapi_guard/__init__.py
-from guard.handlers.cloud_handler import cloud_handler, CloudManager
-from guard.handlers.ipban_handler import ip_ban_manager, IPBanManager
+from guard.handlers.cloud_handler import CloudManager, cloud_handler
+from guard.handlers.ipban_handler import IPBanManager, ip_ban_manager
 from guard.handlers.ipinfo_handler import IPInfoManager
 from guard.handlers.ratelimit_handler import RateLimitHandler, rate_limit_handler
-from guard.handlers.redis_handler import redis_handler, RedisManager
+from guard.handlers.redis_handler import RedisManager, redis_handler
 from guard.middleware import SecurityMiddleware
 from guard.models import SecurityConfig
 
