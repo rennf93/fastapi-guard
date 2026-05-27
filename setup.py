ee806054747b35ae839b8640e377d7c10c4bc470
Renn F <rennf93@users.noreply.github.com>
2026-05-28 00:12:50 +0200

    Cleanup

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    packages=find_packages(include=["guard", "guard.*"]),
-    include_package_data=True,
-    package_data={
-        "guard": ["py.typed"],
-    },
-)
